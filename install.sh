ea20f79aaff1bae762282bd100ed4c89d7913cf2
Niopt <gavriilfedorov19@gmail.com>
2025-07-02 15:10:42 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,10 @@ plain='\033[0m'
 
 cur_dir=$(pwd)
 
+# check root
 [[ $EUID -ne 0 ]] && echo -e "${red}Fatal error: ${plain} Please run this script with root privilege \n " && exit 1
 
+# Check OS and set release variable
 if [[ -f /etc/os-release ]]; then
     source /etc/os-release
     release=$ID
@@ -20,7 +22,6 @@ else
     echo "Failed to check the system OS, please contact the author!" >&2
     exit 1
 fi
-
 echo "The OS release is: $release"
 
 arch() {
@@ -40,6 +41,7 @@ echo "Arch: $(arch)"
 
 check_glibc_version() {
     glibc_version=$(ldd --version | head -n1 | awk '{print $NF}')
+    
     required_version="2.32"
     if [[ "$(printf '%s\n' "$required_version" "$glibc_version" | sort -V | head -n1)" != "$required_version" ]]; then
         echo -e "${red}GLIBC version $glibc_version is too old! Required: 2.32 or higher${plain}"
@@ -109,12 +111,6 @@ config_after_install() {
             echo -e "${green}WebBasePath: ${config_webBasePath}${plain}"
             echo -e "${green}Access URL: http://${server_ip}:${config_port}/${config_webBasePath}${plain}"
             echo -e "###############################################"
-            echo "Username: ${config_username}" >> /root/x-ui-credentials.txt
-            echo "Password: ${config_password}" >> /root/x-ui-credentials.txt
-            echo "Port: ${config_port}" >> /root/x-ui-credentials.txt
-            echo "WebBasePath: ${config_webBasePath}" >> /root/x-ui-credentials.txt
-            echo "Access URL: http://${server_ip}:${config_port}/${config_webBasePath}" >> /root/x-ui-credentials.txt
-            echo "----------------------------------------" >> /root/x-ui-credentials.txt
         else
             local config_webBasePath=$(gen_random_string 15)
             echo -e "${yellow}WebBasePath is missing or too short. Generating a new one...${plain}"
@@ -134,9 +130,6 @@ config_after_install() {
             echo -e "${green}Username: ${config_username}${plain}"
             echo -e "${green}Password: ${config_password}${plain}"
             echo -e "###############################################"
-            echo "Username: ${config_username}" >> /root/x-ui-credentials.txt
-            echo "Password: ${config_password}" >> /root/x-ui-credentials.txt
-            echo "----------------------------------------" >> /root/x-ui-credentials.txt
         else
             echo -e "${green}Username, Password, and WebBasePath are properly set. Exiting...${plain}"
         fi
@@ -189,6 +182,7 @@ install_x-ui() {
     cd x-ui
     chmod +x x-ui
 
+    # Check the system's architecture and rename the file accordingly
     if [[ $(arch) == "armv5" || $(arch) == "armv6" || $(arch) == "armv7" ]]; then
         mv bin/xray-linux-$(arch) bin/xray-linux-arm
         chmod +x bin/xray-linux-arm
@@ -206,7 +200,24 @@ install_x-ui() {
     systemctl start x-ui
     echo -e "${green}x-ui ${tag_version}${plain} installation finished, it is running now..."
     echo -e ""
-    echo -e "\n┌───────────────────────────────────────────────────────┐\n│  ${blue}x-ui control menu usages (subcommands):${plain}              │\n│                                                       │\n│  ${blue}x-ui${plain}              - Admin Management Script          │\n│  ${blue}x-ui start${plain}        - Start                            │\n│  ${blue}x-ui stop${plain}         - Stop                             │\n│  ${blue}x-ui restart${plain}      - Restart                          │\n│  ${blue}x-ui status${plain}       - Current Status                   │\n│  ${blue}x-ui settings${plain}     - Current Settings                 │\n│  ${blue}x-ui enable${plain}       - Enable Autostart on OS Startup   │\n│  ${blue}x-ui disable${plain}      - Disable Autostart on OS Startup  │\n│  ${blue}x-ui log${plain}          - Check logs                       │\n│  ${blue}x-ui banlog${plain}       - Check Fail2ban ban logs          │\n│  ${blue}x-ui update${plain}       - Update                           │\n│  ${blue}x-ui legacy${plain}       - legacy version                   │\n│  ${blue}x-ui install${plain}      - Install                          │\n│  ${blue}x-ui uninstall${plain}    - Uninstall                        │\n└───────────────────────────────────────────────────────┘"
+    echo -e "┌───────────────────────────────────────────────────────┐
+│  ${blue}x-ui control menu usages (subcommands):${plain}              │
+│                                                       │
+│  ${blue}x-ui${plain}              - Admin Management Script          │
+│  ${blue}x-ui start${plain}        - Start                            │
+│  ${blue}x-ui stop${plain}         - Stop                             │
+│  ${blue}x-ui restart${plain}      - Restart                          │
+│  ${blue}x-ui status${plain}       - Current Status                   │
+│  ${blue}x-ui settings${plain}     - Current Settings                 │
+│  ${blue}x-ui enable${plain}       - Enable Autostart on OS Startup   │
+│  ${blue}x-ui disable${plain}      - Disable Autostart on OS Startup  │
+│  ${blue}x-ui log${plain}          - Check logs                       │
+│  ${blue}x-ui banlog${plain}       - Check Fail2ban ban logs          │
+│  ${blue}x-ui update${plain}       - Update                           │
+│  ${blue}x-ui legacy${plain}       - legacy version                   │
+│  ${blue}x-ui install${plain}      - Install                          │
+│  ${blue}x-ui uninstall${plain}    - Uninstall                        │
+└───────────────────────────────────────────────────────┘"
 }
 
 echo -e "${green}Running...${plain}"
